619dc1c75b063ab501cb2faf645bfccf89c65bfc
Tomas Aparicio <tomas@aparicio.me>
2017-01-02 19:24:06 +0000

    fix(tests)

--- tests/async_retrier_context.py
+++ tests/async_retrier_context.py
@@ -3,7 +3,7 @@ import asyncio
 from riprova import AsyncRetrier, ConstantBackoff
 
 
-def test_async_retrier_context_manager(MagicMock, coro):
+def test_async_retrier_context_manager(MagicMock, coro, run_coro):
     on_retry = MagicMock()
     retrier = AsyncRetrier(timeout=.25, on_retry=on_retry,
                            backoff=ConstantBackoff(interval=.1, retries=5))
--- tests/async_retrier_test.py
+++ tests/async_retrier_test.py
@@ -226,28 +226,5 @@ def test_async_retrier_istimeout():
 
 @python34
 def test_async_retrier_context_manager(MagicMock, coro):
-    from .async_retrier_context import *  # noqa
-    test_async_retrier_context_manager()
-    # on_retry = MagicMock()
-    # retrier = AsyncRetrier(timeout=.25, on_retry=on_retry,
-    #                        backoff=ConstantBackoff(interval=.1, retries=5))
-    #
-    # @asyncio.coroutine
-    # def run_context():
-    #     assert (yield from retrier.__aenter__()) is retrier  # noqa
-    #
-    #     try:
-    #         yield from retrier.run(coro(10), 2, 4, foo='bar')  # noqa
-    #     except:
-    #         yield from retrier.__aexit__(None, None, None)  # noqa
-    #     else:
-    #         raise AssertionError('must raise exception')
-    #
-    # run_coro(run_context())
-    #
-    # assert on_retry.called
-    # assert on_retry.call_count == 3
-    #
-    # assert retrier.attempts >= 1
-    # assert retrier.attempts < 4
-    # assert isinstance(retrier.error, RuntimeError)
+    from .async_retrier_context import test_async_retrier_context_manager
+    test_async_retrier_context_manager(MagicMock, coro, run_coro)
