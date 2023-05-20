6fd8da289fcef04cf202d08495830d4aca71f06f
Tom <tomas@aparicio.me>
2023-05-21 00:44:27 +0200

    Update constants.py

--- riprova/constants.py
+++ riprova/constants.py
@@ -3,6 +3,7 @@ import sys
 
 # Store if Python runtime is higher or equal to 3.4
 PY_34 = sys.version_info >= (3, 4)
+PY_35 = sys.version_info >= (3, 5)
 
 # Assertion template errors
 INT_ERROR = '{} param must be an int'
